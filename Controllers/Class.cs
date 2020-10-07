4f76eb9a29b75bf9ea3c6a65a3fe19076a4e2484
viniciusmorgado <contato.viniciusdonatto@gmail.com>
2020-10-07 18:13:30 -0300

    Add test classes for MVC folders

---
+++ Controllers/Class.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PictureGallery.Controllers
+{
+    class Class
+    {
+        // This class is for test only.
+    }
+}
